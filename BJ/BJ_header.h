d54a0134a07ecbff4b67f075022dd99f2b77cb46
antondragos <anton.razvan10@yahoo.com>
2017-01-04 20:08:16 +0200

    Lansarea unei carti

--- BJ/BJ_header.h
+++ BJ/BJ_header.h
@@ -1,5 +1,7 @@
 struct carte{
              unsigned int numar;
              char simbol[20];
+             int aparitie;
 };
 void creare_pachet();
+void randCard(unsigned int & a);
--- BJ/main.cpp
+++ BJ/main.cpp
@@ -1,8 +1,16 @@
 #include"BJ_header.h"
 #include <iostream>
+#include<cstdlib>
+#include<ctime>
 #include<string.h>
 using namespace std;
 carte pachet[52];
+void randCard(unsigned int & a)
+{
+    srand((unsigned)time(0));
+    a= (rand()%52);
+
+}
 void creare_pachet()
 {unsigned int i;
 for(i=0;i<52;i++)
@@ -12,7 +20,20 @@ else if(i<26) strcpy(pachet[i].simbol,"romb");
 else if(i<39)strcpy(pachet[i].simbol,"frunza");
 else strcpy(pachet[i].simbol,"trefla");
 }}
+int lansare_carte()
+{unsigned int c;
+randCard(c);
+while (pachet[c].aparitie==1)
+    randCard(c);
+    pachet[c].aparitie=1;
+return c+1;
+}
 
 int main()
-{
+{unsigned int i,b;
+creare_pachet();
+for(i=0;i<52;i++)
+    pachet[i].aparitie=0;
+  for(i=0;i<52;i++)  cout<<lansare_carte()<<endl;
+
 }
