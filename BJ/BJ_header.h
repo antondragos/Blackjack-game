f2d5f0bf8a5a4df0b18faee7979d88b764b05c03
antondragos <anton.razvan10@yahoo.com>
2017-01-04 19:22:55 +0200

    Header

---
+++ BJ/BJ_header.h
@@ -0,0 +1,5 @@
+struct carte{
+             unsigned int numar;
+             char simbol[20];
+};
+void creare_pachet();
--- BJ/main.cpp
+++ BJ/main.cpp
@@ -1,10 +1,7 @@
+#include"BJ_header.h"
 #include <iostream>
 #include<string.h>
 using namespace std;
-struct carte{
-             unsigned int numar;
-             char simbol[20];
-};
 carte pachet[52];
 void creare_pachet()
 {unsigned int i;
@@ -15,9 +12,7 @@ else if(i<26) strcpy(pachet[i].simbol,"romb");
 else if(i<39)strcpy(pachet[i].simbol,"frunza");
 else strcpy(pachet[i].simbol,"trefla");
 }}
+
 int main()
-{ creare_pachet();
-    for(int i=0;i<52;i++)
-cout<<pachet[i].numar<<"->"<<pachet[i].simbol<<endl;
-return 0;
+{
 }
