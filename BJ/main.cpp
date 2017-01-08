5122c07c7f2e00b6385f2ef92066599d24b682d0
antondragos <anton.razvan10@yahoo.com>
2017-01-08 16:01:23 +0200

    Impartirea primelor carti in modul de joc contra calculatorului

--- BJ/main.cpp
+++ BJ/main.cpp
@@ -26,14 +26,36 @@ randCard(c);
 while (pachet[c].aparitie==1)
     randCard(c);
     pachet[c].aparitie=1;
-return c+1;
+return c;
 }
-
+void initializareFrecventa()
+{int i;
+    for(i=0;i<52;i++)
+    pachet[i].aparitie=0;
+}
 int main()
 {unsigned int i,b;
+unsigned int choice;
 creare_pachet();
-for(i=0;i<52;i++)
-    pachet[i].aparitie=0;
-  for(i=0;i<52;i++)  cout<<lansare_carte()<<endl;
+initializareFrecventa();
+cout<<"Alegeti modul de joc pe care il doriti"<<endl<<"Press 1 for playing with computer"<<endl<<"Press 2 for playing with your friend"<<endl<<"Alegerea dumneavoastra:";
+cin>>choice;
+cout<<endl;
+if (choice==1)
+{int card,card2;
+card=lansare_carte();
+card2=lansare_carte();
+    cout<<"Dealer's hand:";
+    cout<<pachet[card].numar<<"->"<<pachet[card].simbol;
+    cout<<" ";
+    cout<<pachet[card2].numar<<"->"<<pachet[card2].simbol<<endl;
+card=lansare_carte();
+card2=lansare_carte();
+    cout<<"Your hand:";
+    cout<<pachet[card].numar<<"->"<<pachet[card].simbol;
+    cout<<" ";
+    cout<<pachet[card2].numar<<"->"<<pachet[card2].simbol<<endl;
+}
+return 0;
 
 }
